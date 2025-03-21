8fce500874e7d4cf6ea0bf9f656e97c831ba4782
Dan Parkes <dan.parkes15@gmail.com>
2025-03-21 12:19:04 +0000

    corrected silakka ws switch keys

--- keyboards/silakka54/keymaps/daparkes/keymap.c
+++ keyboards/silakka54/keymaps/daparkes/keymap.c
@@ -24,7 +24,7 @@ __const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,     KC_F8,     KC_F9,   KC_F10,  KC_F11,
         KC_TRNS,  KC_TRNS, KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS,                            KC_COPY, KC_LCBR,   KC_RCBR,   KC_LBRC, KC_RBRC,  KC_F12,
         KC_TRNS,  KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY,                            KC_LEFT, KC_DOWN,   KC_UP,     KC_RGHT, KC_EQL,  KC_PLUS,
-        WS_LFT,   KC_TRNS, KC_TRNS, WS_RGT,  KC_TRNS, KC_TRNS,                            KC_PAS,  KC_PIPE,   KC_NUHS,   KC_MINS, KC_UNDS, KC_TRNS,
+        KC_TRNS,  WS_LFT,  KC_TRNS, KC_TRNS, WS_RGT,  KC_TRNS,                            KC_PAS,  KC_PIPE,   KC_NUHS,   KC_MINS, KC_UNDS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     )
 };
